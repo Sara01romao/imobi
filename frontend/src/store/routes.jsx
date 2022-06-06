9c3cfa21171682a09025c7c0d3446ebcc57e1dd0
Sara Romão <sara-romao@live.com>
2022-06-05 23:41:44 -0300

    header e rota de lista de imoveis

--- frontend/src/store/routes.jsx
+++ frontend/src/store/routes.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {Header} from "../components/Header";
 import { Home } from "../pages/Home";
+import { ListaImoveis } from "../pages/Lista-Imoveis";
 
 const Routas=()=>{
     return(
     <BrowserRouter>
+       <Header/>
         <Routes>
            <Route path="/" element={<Home/>}/>
+           <Route path="/lista-imoveis" element={<ListaImoveis/>}/>
         </Routes>
     
     </BrowserRouter>
