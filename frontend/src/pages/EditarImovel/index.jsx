8988d3ab736a7b49962bb8009c5a794c43dee647
Sara Romão <sara-romao@live.com>
2022-06-07 18:22:17 -0300

    estilo

--- frontend/src/pages/EditarImovel/index.jsx
+++ frontend/src/pages/EditarImovel/index.jsx
@@ -1,13 +1,61 @@
-import React, { useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Input } from "../../components/Input";
 
 export default function EditarImovel() {
-  const {id} = useParams();
+  const { id } = useParams();
+  const [imovel, setImovel] = useState({});
 
-  useEffect(() =>{
-    
-  }, [])
+  const [titulo, setTitulo] = useState("");
+  const [descricao, setDescricao] = useState("");
+  const [preco, setPreco] = useState("");
+  const [data, setData] = useState("");
+
+  function editar(){
+
+  }
+
+  useEffect(() => {
+
+  }, []);
+
   return (
-    <div>EditarImovel {id}</div>
-  )
+    <div>
+      <h1>Editar Imóvel</h1>
+      <form action="" onSubmit={editar}>
+        <div className="divInput">
+          <div>
+             <label htmlFor="titulo">Título Imóvel</label>
+             <p>cód:{id}</p>
+          </div>
+          
+          <Input type="text" id="titulo" value={titulo} setChange={setTitulo} />
+        </div>
+
+        <div className="divInput">
+          <label htmlFor="preco">Preço</label>
+          <Input type="number" id="preco" value={preco} setChange={setPreco} />
+        </div>
+        <div className="divInput">
+          <label htmlFor="data">Data</label>
+          <Input type="date" id="data" value={data} setChange={setData} />
+        </div>
+
+        <div className="divInput">
+          <label htmlFor="descricao">Descrição</label>
+          <textarea
+            id="descricao"
+            value={descricao}
+            onChange={(e) => setDescricao(e.target.value)}
+            cols="30"
+            rows="5"
+            style={{ resize: "none" }}
+          ></textarea>
+          
+        </div>
+
+        <button> Enviar</button>
+      </form>
+    </div>
+  );
 }
