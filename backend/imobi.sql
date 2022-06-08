d16a368144fbf5653cad7b3fdfc59e4e250aaeb8
Sara Romão <sara-romao@live.com>
2022-06-08 01:19:14 -0300

    alteração nos valores

--- backend/imobi.sql
+++ backend/imobi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Tempo de geração: 08-Jun-2022 às 05:56
+-- Tempo de geração: 08-Jun-2022 às 06:17
 -- Versão do servidor: 10.4.24-MariaDB
 -- versão do PHP: 7.4.28
 
@@ -40,12 +40,12 @@ CREATE TABLE `imoveis` (
 --
 
 INSERT INTO `imoveis` (`id`, `titulo`, `descricao`, `preco`, `data`) VALUES
-(38, 'Apartamento', '1 quartos, 1 banheiro, sala, cozinha.', 51, '2022-06-10'),
-(45, 'Apartamento', '3 quartos, 1 banheiro, sala e cozinha', 280, '2022-06-20'),
-(46, 'Apartamento', '2 quartos, 1 banheiro, sala e cozinha', 188, '2022-06-30'),
-(49, 'Casa', '4 quartos, 2 banheiro, sala, cozinha.', 350, '2022-03-09'),
-(55, 'Casa', '4 quartos, 2 banheiro, sala, cozinha.', 550, '2022-06-16'),
-(62, 'Casa Rosa', '2 quartos, 1 banheiro, sala e cozinha', 140, '2022-06-30');
+(38, 'Apartamento', '1 quartos, 1 banheiro, sala, cozinha.', 51000, '2022-06-10'),
+(45, 'Apartamento', '3 quartos, 1 banheiro, sala e cozinha', 280000, '2022-06-20'),
+(46, 'Apartamento', '2 quartos, 1 banheiro, sala e cozinha', 188000, '2022-06-30'),
+(49, 'Casa', '4 quartos, 2 banheiro, sala, cozinha.', 350000, '2022-03-09'),
+(55, 'Casa', '4 quartos, 2 banheiro, sala, cozinha.', 550000, '2022-06-16'),
+(62, 'Casa Rosa', '2 quartos, 1 banheiro, sala e cozinha', 140000, '2022-06-30');
 
 --
 -- Índices para tabelas despejadas
